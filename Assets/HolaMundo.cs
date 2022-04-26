e12180a4e29a55e26770409a228e61705d70879c
AryBacher <47170979@est.ort.edu.ar>
2022-04-26 09:07:21 -0300

    Finished

--- Assets/HolaMundo.cs
+++ Assets/HolaMundo.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class HolaMundo : MonoBehaviour
 {
+    public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,27 @@ public class HolaMundo : MonoBehaviour
 
     public void holaWorld()
     {
-        Debug.Log("Hola Mundo");
+        if (num1 == num2 || num1 < num2)
+        {
+            Debug.Log("Error, Asegúrese que el Primer Número sea Mayor que el Segundo");
+        }
+
+        else
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num1);
+                num1--;
+            }
+        }
+
+        /*int i = 1;
+        while (i < 100)
+        {
+            Debug.Log(i);
+            i++;
+        }*/
+       
+
     }
 }
